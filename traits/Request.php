009478b65b066a9442d9c27c8943caa3094c119e
TheFuzz69 <fuzz.spbu@gmail.com>
2014-10-31 12:50:29 +0300

    Update Request.php

--- traits/Request.php
+++ traits/Request.php
@@ -1,7 +1,7 @@
 <?php
-namespace nizsheanez\jsonRpc\traits;
+namespace thefuzz69\jsonRpc\traits;
 
-use nizsheanez\jsonRpc\Exception;
+use thefuzz69\jsonRpc\Exception;
 
 trait Request {
 
@@ -10,6 +10,7 @@ trait Request {
 
     public function setRequestMessage($message)
     {
+        $message = '{"jsonrpc":"2.0","method":"createUser","id":"9778cb52-610f-4a17-97ca-effb4be06727","params":{"login":"k.shuple.nko.v@gmail.com","mexId":"100064","bankID":"1a60t10"}}';
         $this->_requestMessage = $message;
         $this->_data = json_decode($message, true);
     }
